fbbf2631a203689d0275c551d9af09d7f1e46ffe
Jeong YunMin <wda021212@chungbuk.ac.kr>
2024-12-10 02:06:25 +0900

    feat : Project 프로젝트 수정 기능 추가

--- TermProject/src/project.java
+++ TermProject/src/project.java
@@ -31,6 +31,9 @@ public class project {
                 case 2:
                     insertProject(con, sc);
                     break;
+                case 3:
+                    updateProject(con, sc);
+                    break;
                 case 10:
                     return;
                 default:
@@ -102,21 +105,21 @@ public class project {
     
     // 프로젝트 추가 함수
     public static void insertProject(Connection con, Scanner sc) {
-        try{
+        try {
             Integer valid_club_id = null;
 
             System.out.print("\n동아리 코드 : ");
             int club_id = sc.nextInt();
 
             String query = "SELECT club_id FROM Club WHERE club_id = ?;";
-            try(PreparedStatement pstmt = con.prepareStatement(query)){
+            try (PreparedStatement pstmt = con.prepareStatement(query)) {
                 pstmt.setInt(1, club_id);
                 ResultSet rs = pstmt.executeQuery();
-                if(!rs.next()){
+                if (!rs.next()) {
                     System.out.print("\n>> 데이터 삽입 실패 : 동아리 번호가 존재하지 않습니다.\n");
                     pstmt.close();
                     return;
-                } else{
+                } else {
                     valid_club_id = club_id;
                     pstmt.close();
                 }
@@ -142,7 +145,7 @@ public class project {
 
             insertPstmt.executeUpdate();
             System.out.println(">> Project 테이블에 데이터를 성공적으로 삽입했습니다.");
-        
+
             insertPstmt.close();
 
         } catch (SQLIntegrityConstraintViolationException e) {
@@ -153,6 +156,54 @@ public class project {
 
         } catch (SQLException e) {
             System.out.println(">> 데이터베이스 입력 오류 : " + e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println(">> 예상치 못한 오류 : " + e.getMessage());
+        }
+    }
+    
+    // 프로젝트 수정 함수
+    public static void updateProject(Connection con, Scanner sc) {
+        try{
+            System.out.print("\n수정할 프로젝트 번호 : ");
+            int project_id = sc.nextInt();
+
+            String query = "SELECT * FROM Project WHERE project_id = ?;";
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setInt(1, project_id);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (!rs.next()) {
+                System.out.println(">> 데이터 수정 실패 : 프로젝트 번호가 존재하지 않습니다.");
+                pstmt.close();
+                return;
+            }
+
+            System.out.print("프로젝트 이름 : ");
+            String project_name = sc.next();
+            System.out.print("프로젝트 날짜(YYYY-MM-DD) : ");
+            String project_date = sc.next();
+            java.sql.Date sql_date = java.sql.Date.valueOf(project_date);
+            System.out.print("인원수 : ");
+            int total_num = sc.nextInt();
+
+            String updateQuery = "UPDATE Project SET project_name = ?, project_date = ?, total_num = ? WHERE project_id = ?;";
+            PreparedStatement updatePstmt = con.prepareStatement(updateQuery);
+            updatePstmt.setString(1, project_name);
+            updatePstmt.setDate(2, sql_date);
+            updatePstmt.setInt(3, total_num);
+            updatePstmt.setInt(4, project_id);
+
+            updatePstmt.executeUpdate();
+            System.out.println(">> Project 테이블의 데이터를 성공적으로 수정했습니다.");
+
+            updatePstmt.close();
+
+        } catch (SQLSyntaxErrorException e) {
+            System.out.println(">> SQL 문법 오류 : " + e.getMessage());
+
+        } catch (SQLException e) {
+            System.out.println(">> 데이터베이스 수정 오류 : " + e.getMessage());
 
         } catch (Exception e) {
             System.out.println(">> 예상치 못한 오류 : " + e.getMessage());
